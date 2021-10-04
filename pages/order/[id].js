84b7f9dcfe8d3c6246fb2ba5b6e257636d1283de
spiceydev <spiceydev@gmail.com>
2021-10-04 14:36:34 +1300

    implemented making orders delivered

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -12,6 +12,7 @@ import {
   TableHead,
   TableRow,
   Typography,
+  Button,
 } from '@material-ui/core';
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import axios from 'axios';
@@ -23,6 +24,10 @@ import { useSnackbar } from 'notistack';
 import React, { useContext, useEffect, useReducer } from 'react';
 import Layout from '../../components/Layout';
 import {
+  DELIVER_FAIL,
+  DELIVER_REQUEST,
+  DELIVER_RESET,
+  DELIVER_SUCCESS,
   FETCH_FAIL,
   FETCH_REQUEST,
   FETCH_SUCCESS,
@@ -53,6 +58,19 @@ function reducer(state, action) {
       return { ...state, loadingPay: false, errorPay: action.payload };
     case PAY_RESET:
       return { ...state, loadingPay: false, successPay: false, errorPay: '' };
+    case DELIVER_REQUEST:
+      return { ...state, loadingDeliver: true };
+    case DELIVER_SUCCESS:
+      return { ...state, loadingDeliver: false, successDeliver: true };
+    case DELIVER_FAIL:
+      return { ...state, loadingDeliver: false, errorDeliver: action.payload };
+    case DELIVER_RESET:
+      return {
+        ...state,
+        loadingDeliver: false,
+        successDeliver: false,
+        errorDeliver: '',
+      };
     default:
       state;
   }
@@ -66,14 +84,14 @@ const Order = ({ params }) => {
   const { state } = useContext(Store);
   const { userInfo } = state;
 
-  const [{ loading, error, order, successPay }, dispatch] = useReducer(
-    reducer,
-    {
-      loading: true,
-      order: {},
-      error: '',
-    }
-  );
+  const [
+    { loading, loadingDeliver, error, order, successPay, successDeliver },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: '',
+  });
   const {
     shippingAddress,
     paymentMethod,
@@ -103,10 +121,18 @@ const Order = ({ params }) => {
         dispatch({ type: FETCH_FAIL, payload: getError(err) });
       }
     };
-    if (!order._id || successPay || (order._id && order._id !== orderId)) {
+    if (
+      !order._id ||
+      successPay ||
+      successDeliver ||
+      (order._id && order._id !== orderId)
+    ) {
       fetchOrder();
       if (successPay) {
-        dispatch({ type: 'PAY_RESET' });
+        dispatch({ type: PAY_RESET });
+      }
+      if (successDeliver) {
+        dispatch({ type: DELIVER_RESET });
       }
     } else {
       const loadPaypalScript = async () => {
@@ -125,9 +151,27 @@ const Order = ({ params }) => {
       loadPaypalScript();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [order, successPay]);
+  }, [order, successPay, successDeliver]);
 
   const { enqueueSnackbar } = useSnackbar();
+
+  const deliverOrderHandler = async () => {
+    try {
+      dispatch({ type: DELIVER_REQUEST });
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: DELIVER_SUCCESS, payload: data });
+      enqueueSnackbar('Order is delivered', { variant: 'success' });
+    } catch (err) {
+      dispatch({ type: DELIVER_FAIL, payload: getError(err) });
+      enqueueSnackbar(getError(err), { variant: 'error' });
+    }
+  };
 
   function createOrder(data, actions) {
     return actions.order
@@ -365,6 +409,19 @@ const Order = ({ params }) => {
                         ></PayPalButtons>
                       </div>
                     )}
+                  </ListItem>
+                )}
+                {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                  <ListItem>
+                    {loadingDeliver && <CircularProgress />}
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      onClick={deliverOrderHandler}
+                    >
+                      Set as Delivered
+                    </Button>
                   </ListItem>
                 )}
               </List>
